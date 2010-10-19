1933bf9e8166d0a31e1131c1cd7d2d1f377257e8
Sean Cashin <scashin133@gmail.com>
2010-10-19 11:54:28 -0700

    Passing config from binary call.

--- lib/classify_cluster.rb
+++ lib/classify_cluster.rb
@@ -1,7 +1,9 @@
 module ClassifyCluster
   require 'yaml'
   require 'active_support'
+  
   DEFAULT_CONFIG_LOCATION = File.join('/etc', 'cluster.yml')
+  
   def self.get_config(hostname, options={})
     options.reverse_merge! :config_file => DEFAULT_CONFIG_LOCATION
     YAML::dump(cluster_config)
