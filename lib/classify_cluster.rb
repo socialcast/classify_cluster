754524c798d4b696e91ebb1f99672bca27280a0b
Sean Cashin <scashin133@gmail.com>
2010-10-19 11:43:31 -0700

    Reads in yaml and spits it back out.

--- lib/classify_cluster.rb
+++ lib/classify_cluster.rb
@@ -1,3 +1,14 @@
 module ClassifyCluster
+  require 'yaml'
+  DEFAULT_CONFIG_LOCATION = File.join('/etc', 'cluster.yml')
+  def self.get_config(hostname, options={})
+    options.reverse_merge! :config_file => DEFAULT_CONFIG_LOCATION
+    YAML::dump(cluster_config)
+  end
   
+  private
+  
+  def self.cluster_config(config_file)
+    @@cluster_config ||= YAML.load_file(config_file)
+  end
 end
