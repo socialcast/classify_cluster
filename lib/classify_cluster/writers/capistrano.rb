e7120354533108f39bf1274236bc5e72d27129f5
Sean Cashin <scashin133@gmail.com>
2010-11-11 09:18:36 -0800

    turning into valid ruby variables. populating puppet_master variable in capistrano.

--- lib/classify_cluster/writers/capistrano.rb
+++ lib/classify_cluster/writers/capistrano.rb
@@ -16,13 +16,19 @@ module ClassifyCluster
       def self.export!(capistrano_configurator, cluster, config_file=ClassifyCluster::Base.default_config_file)
         config = ClassifyCluster::Configurator::Configuration.new(config_file).clusters[cluster]
         config.variables.each_pair do |name, value|
-          capistrano_configurator.set("puppet-#{name}".to_sym, value)
+          capistrano_configurator.set("puppet_#{name}".to_sym, value)
         end
         config.nodes.each_pair do |name, node|
           roles = node.roles
           next if roles.empty?
+          
           roles.each do |role|
-            capistrano_configurator.role((CLUSTER_ROLE_2_CAP_ROLE[role.type.to_s] || role.type.to_s), node.fqdn, role.options)
+            role_name = (CLUSTER_ROLE_2_CAP_ROLE[role.type.to_s] || role.type.to_s)
+            capistrano_configurator.role(role_name, node.fqdn, role.options)
+            if role_name == 'puppet_master'
+              capistrano_configurator.set("puppet_role_puppet_master_fqdn", node.fqdn)
+              capistrano_configurator.set("puppet_role_puppet_master_private_ip", node.private_ip)
+            end
           end
         end
       end
